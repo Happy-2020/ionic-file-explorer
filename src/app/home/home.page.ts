79761228bcc39e5286f811650d6084617915efed
WenchengLi-WL <babystar6666@outlook.com>
2019-12-20 01:06:36 +0900

    Create new Folders and Files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { File, Entry } from '@ionic-native/file/ngx';
+import { Platform, AlertController, ToastController } from '@ionic/angular';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    directories = [];
+    folder = '';
+    copyFile: Entry = null;
+    shouldMove = false;
 
-  constructor() {}
+    constructor(
+        private file: File,
+        private plt: Platform,
+        private alertCtrl: AlertController,
+        private fileOpener: FileOpener,
+        private router: Router,
+        private route: ActivatedRoute,
+        private toastCtrl: ToastController
+    ) {}
+
+    ngOnInit()
+    {
+        this.folder = this.route.snapshot.paramMap.get('folder') || '';
+        this.loadDocuments();
+    }
+
+    loadDocuments()
+    {
+        this.plt.ready().then(() => {
+            // Reset for later copy / move operations
+            this.copyFile = null;
+            this.shouldMove = false;
+
+            this.file.listDir(this.file.dataDirectory, this.folder).then(res => {
+                this.directories = res;
+            });
+        });
+    }
+
+    async createFolder()
+    {
+        let alert = await this.alertCtrl.create({
+            header: 'Create folder',
+            message: 'Please specify the name of the new folder',
+            inputs: [
+                {
+                    name: 'name',
+                    type: 'text',
+                    placeholder: 'MyDir'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                },
+                {
+                    text: 'Create',
+                    handler: data => {
+                        this.file.createDir(
+                            `${this.file.dataDirectory}/${this.folder}`,
+                            data.name,
+                            false
+                        ).then(res => {
+                            this.loadDocuments();
+                        });
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    async createFile()
+    {
+        let alert = await this.alertCtrl.create({
+            header: 'Create file',
+            message: 'Please specify the name of the new file',
+            inputs: [
+                {
+                    name: 'name',
+                    type: 'text',
+                    placeholder: 'MyFile'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                },
+                {
+                    text: 'Create',
+                    handler: data => {
+                        this.file.writeFile(
+                            `${this.file.dataDirectory}/${this.folder}`,
+                            `${data.name}.txt`,
+                            `My custom text - ${new Date().getTime()}`
+                        ).then(res => {
+                            this.loadDocuments();
+                        });
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
 
 }
